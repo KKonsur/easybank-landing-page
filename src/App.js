d7a25055ffd6ecf7bc24ac6e5b79cbb2d8c2dfad
KKonsur <konradkonsur@gmail.com>
2022-10-06 21:54:49 +0200

    complete main section

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import './App.scss'
 import Header from './components/Header/Header';
 import Navigation from './components/Navigation/Navigation';
 import logo from './images/logo.svg'
+import imageMockups from './images/image-mockups.png'
 import Button from './UI/Button/Button';
+import Main from './components/Main/Main';
 
 function App() {
   return (
@@ -10,8 +12,18 @@ function App() {
       <Header>
         <img className='header__logo' src={logo} alt="logo" />
         <Navigation />
-        <Button name={'Request Invite'}/>
+        <Button name={'Request Invite'} />
       </Header>
+      <Main>
+        <div className="main__content">
+          <h1 className='main__heading'>Next generation digital banking</h1>
+          <p className='main__paragraph'>Take your financial life online. Your Easybank account will be a one-stop-shop for spending, saving, budgeting, investing, and much more.</p>
+          <Button name={'Request Invite'} />
+        </div>
+        <div className="main__images">
+          <img className="main__mockups" src={imageMockups} alt="" />
+        </div>
+      </Main>
     </div>
   );
 }
